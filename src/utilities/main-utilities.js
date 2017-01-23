bade35c778951a0d05733da164e43999239bb026
metincansiper <metincansiper@gmail.com>
2017-01-23 22:57:39 +0300

    Refactoring chise at the top of sbgnviz
    Adding new main utilities to directly expose

--- src/utilities/main-utilities.js
+++ src/utilities/main-utilities.js
@@ -1,20 +1,20 @@
 /*
  * The main utilities to be exposed directly.
  */
-function mainUtilities(){
-};
+function mainUtilities() {
+}
+;
 
 /*
  * Clone given elements. Considers undoable option. Requires cytoscape-clipboard extension.
  */
-mainUtilities.cloneElements = function(eles) {
+mainUtilities.cloneElements = function (eles) {
   var cb = cy.clipboard();
   var _id = cb.copy(eles, "cloneOperation");
-  
-  if(options.undoable) {
+
+  if (options.undoable) {
     cy.undoRedo().do("paste", {id: _id});
-  }
-  else {
+  } else {
     cb.paste(_id);
   }
 };
@@ -25,7 +25,7 @@ mainUtilities.cloneElements = function(eles) {
  * alignTo parameter indicates the leading node.
  * Requrires cytoscape-grid-guide extension and considers undoable option.
  */
-mainUtilities.align = function(nodes, horizontal, vertical, alignTo) {
+mainUtilities.align = function (nodes, horizontal, vertical, alignTo) {
   if (options.undoable) {
     cy.undoRedo().do("align", {
       nodes: nodes,
@@ -33,9 +33,45 @@ mainUtilities.align = function(nodes, horizontal, vertical, alignTo) {
       vertical: vertical,
       alignTo: alignTo
     });
-  }
-  else {
+  } else {
     nodes.align(horizontal, vertical, alignTo);
   }
 };
 
+/*
+ * Create compound for given nodes. compoundType may be 'complex' or 'compartment'.
+ * This method considers undoable option.
+ */
+mainUtilities.createCompoundForGivenNodes = function (_nodes, compoundType) {
+  var nodes = _nodes;
+  // Just EPN's can be included in complexes so we need to filter EPN's if compound type is complex
+  if (compoundType === 'complex') {
+    nodes = _nodes.filter(function (i, element) {
+      var sbgnclass = element.data("class");
+      return elementUtilities.isEPNClass(sbgnclass);
+    });
+  }
+  
+  nodes = elementUtilities.getTopMostNodes(nodes);
+
+  // All elements should have the same parent and the common parent should not be a 'complex' 
+  // if compoundType is 'compartent'
+  // because the old common parent will be the parent of the new compartment after this operation and
+  // 'complexes' cannot include 'compartments'
+  if (nodes.length == 0 || !elementUtilities.allHaveTheSameParent(nodes)
+          || ( compoundType === 'compartment' && nodes.parent().data('class') === 'complex' ) ) {
+    return;
+  }
+  
+  if (cy.undoRedo()) {
+    var param = {
+      compundType: compoundType,
+      nodesToMakeCompound: nodes
+    };
+
+    cy.undoRedo().do("createCompoundForGivenNodes", param);
+  }
+  else {
+    elementUtilities.createCompoundForGivenNodes(nodes, compoundType);
+  }
+};
