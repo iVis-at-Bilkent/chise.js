4c4223359b5fb75d4d112e8f2097a44e88103a36
kinimesi <kinimesi@users.noreply.github.com>
2017-06-23 12:08:59 +0300

    Fixes https://github.com/iVis-at-Bilkent/newt/issues/92

--- src/utilities/element-utilities.js
+++ src/utilities/element-utilities.js
@@ -1099,7 +1099,7 @@ elementUtilities.canHaveSBGNCardinality = function (ele) {
 elementUtilities.canHaveSBGNLabel = function (ele) {
   var sbgnclass = typeof ele === 'string' ? ele : ele.data('class');
 
-  return sbgnclass != 'and' && sbgnclass != 'or' && sbgnclass != 'not'
+  return sbgnclass != 'and' && sbgnclass != 'or' && sbgnclass != 'not' && sngnclass != 'delay'
           && sbgnclass != 'association' && sbgnclass != 'dissociation' && !sbgnclass.endsWith('process');
 };
 
